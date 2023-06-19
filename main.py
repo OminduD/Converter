7c1e07a3934a79e2994882d6dd6bba89d4da999f
OminduD <89883687+OminduD@users.noreply.github.com>
2023-06-19 10:39:28 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-a=0
-b=0
-x=0
-y=1
-while y==1:
+import math
+a = 0
+b = 0
+x = 0
+y = 1
+z = 9/5
+h = math.pi
+while y == 1:
  print('******************###########################$$$$$ _Converter_ $$$$$###########################******************')
  print('''
 Temperature = 1
 Length      = 2
 Weight      = 3
+Angles      = 4
 ''')
  a = int(input('Input Your Converter Type:= '))
- while a >= 4:
+ while a >= 5:
     print('Invalid Number')
     a = int(input('Input Your Converter Type Again:= '))
- if a==1:
+ if a == 1:
     print('''################################################_Temperature_################################################
 Celsius to Kelvin   = 1
 Kelvin to Celsius   = 2
@@ -25,36 +29,36 @@ Kelvin to Fahr.     = 6''')
     while b >= 7:
         print('Invalid Number')
         b = int(input('Input Your Converter Type Again:= '))
-    if b==1:
+    if b == 1:
         x = int(input('Input Your Temperature:= '))
-        x+=273
+        x += 273
         print('Your Temperature:=', x, 'K')
-    elif b==2:
+    elif b == 2:
         x = int(input('Input Your Temperature:= '))
-        x-= 273
+        x -= 273
         print('Your Temperature:=', x, '`C')
-    elif b==3:
+    elif b == 3:
         x = int(input('Input Your Temperature:= '))
-        x=1.8*x+32
+        x = z*x+32
         print('Your Temperature:=', x, '`F')
-    elif b==4:
+    elif b == 4:
         x = int(input('Input Your Temperature:= '))
-        x=0.556*x-17.777
+        x = x/z-32/y
         print('Your Temperature:=', x, '`C')
-    elif b==5:
+    elif b == 5:
         x = int(input('Input Your Temperature:= '))
-        x=1.*x-459.4
+        x = z*x-459.4
         print('Your Temperature:=', x, 'K')
-    elif b==6:
+    elif b == 6:
         x = int(input('Input Your Temperature:= '))
-        x=0.556*x+255.23
+        x = x/y-32/z+273
         print('Your Temperature:=', x, '`F')
     print('''
 Main Menu   = 1
 Exit        = 2
 ''')
-    y=int(input('Enter Your Number:= '))
- elif a==2:
+    y = int(input('Enter Your Number:= '))
+ elif a == 2:
     print('''################################################_Length_################################################
 Miles to Kilometre  = 1
 Kilometre to Miles  = 2
@@ -69,61 +73,69 @@ Centimetre to Inch  = 8
     while b >= 9:
         print('Invalid Number')
         b = int(input('Input Your Converter Type Again:= '))
-    if b==1:
+    if b == 1:
         x = int(input('Input Your Length:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Length Again:= '))
-        x=1.6*x
+            x = abs(x)
+        x = 1.6*x
         print('Your Length:=', x, 'km')
-    elif b==2:
+    elif b == 2:
         x = int(input('Input Your Length:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Length Again:= '))
-        x=x/1.6
+            x = abs(x)
+        x = x/1.6
         print('Your Length:=', x, 'miles')
-    elif b==3:
+    elif b == 3:
         x = int(input('Input Your Length:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Length Again:= '))
-        x=x*1.094
+            x = abs(x)
+        x = x*1.094
         print('Your Length:=', x, 'm')
-    elif b==4:
+    elif b == 4:
         x = int(input('Input Your Length:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Length Again:= '))
-        x=x/1.094
+            x = abs(x)
+        x = x/1.094
         print('Your Length:=', x, 'yard')
-    elif b==5:
+    elif b == 5:
         x = int(input('Input Your Length:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Length Again:= '))
-        x=x*3.281
+            x = abs(x)
+        x = x*3.281
         print('Your Length:=', x, 'm')
-    elif b==6:
+    elif b == 6:
         x = int(input('Input Your Length:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Length Again:= '))
-        x=x/3.281
+            x = abs(x)
+        x = x/3.281
         print('Your Length:=', x, 'foot')
-    elif b==7:
+    elif b == 7:
         x = int(input('Input Your Length:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Length Again:= '))
-        x=x*2.54
+            x = abs(x)
+        x = x*2.54
         print('Your Length:=', x, 'cm')
-    elif b==8:
+    elif b == 8:
         x = int(input('Input Your Length:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Length Again:= '))
-        x=x/2.54
+            x = abs(x)
+        x = x/2.54
         print('Your Length:=', x, 'inch')
     print('''
 Main Menu   = 1
@@ -131,7 +143,7 @@ Exit        = 2
 ''')
     y = int(input('Enter Your Number:= '))
 
- elif a==3:
+ elif a == 3:
     print('''################################################_Weight_################################################
 Pound to Kilogram   = 1
 Kilogram to Pound   = 2
@@ -148,74 +160,81 @@ Kilogram to Stone   = 10
     while b >= 11:
         print('Invalid Number')
         b = int(input('Input Your Converter Type Again:= '))
-    if b==1:
+    if b == 1:
         x = int(input('Input Your Weight:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Weight Again:= '))
-        x=x/2.205
+        x = x/2.205
         print('Your Weight:=', x, 'kg')
-    elif b==2:
+    elif b == 2:
         x = int(input('Input Your Weight:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Weight Again:= '))
-        x=x*2.205
+        x = x*2.205
         print('Your Weight:=', x, 'pound')
-    elif b==3:
+    elif b == 3:
         x = int(input('Input Your Weight:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Weight Again:= '))
-        x=x/5
+        x = x/5
         print('Your Weight:=', x, 'g')
-    elif b==4:
+    elif b == 4:
         x = int(input('Input Your Weight:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Weight Again:= '))
-        x=x*5
+            x = abs(x)
+        x = x*5
         print('Your Weight:=', x, 'carat')
-    elif b==5:
+    elif b == 5:
         x = int(input('Input Your Weight:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Weight Again:= '))
-        x=x*28.35
+            x = abs(x)
+        x = x*28.35
         print('Your Weight:=', x, 'g')
-    elif b==6:
+    elif b == 6:
         x = int(input('Input Your Weight:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Weight Again:= '))
-        x=x/28.35
+            x = abs(x)
+        x = x/28.35
         print('Your Weight:=', x, 'ounce')
-    elif b==7:
+    elif b == 7:
         x = int(input('Input Your Weight:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Weight Again:= '))
-        x=x*31.103
+            x = abs(x)
+        x = x*31.103
         print('Your Weight:=', x, 'g')
-    elif b==8:
+    elif b == 8:
         x = int(input('Input Your Weight:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Weight Again:= '))
-        x=x/31.103
+            x = abs(x)
+        x = x/31.103
         print('Your Weight:=', x, 'troy')
-    elif b==9:
+    elif b == 9:
         x = int(input('Input Your Weight:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Weight Again:= '))
-        x=x*6.35
+            x = abs(x)
+        x = x*6.35
         print('Your Weight:=', x, 'kg')
-    elif b==10:
+    elif b == 10:
         x = int(input('Input Your Weight:= '))
-        while x<0:
+        while x < 0:
             print('Please Input Correct Number')
             x = int(input('Input Your Weight Again:= '))
+            x = abs(x)
         x = x/6.35
         print('Your Weight:=', x, 'stone')
     print('''
@@ -223,3 +242,27 @@ Main Menu   = 1
 Exit        = 2
 ''')
     y = int(input('Enter Your Number:= '))
+ elif a == 4:
+    print('''################################################_Angle_################################################
+Degrees to Radient  = 1
+Radient to Degrees  = 2
+
+''')
+    b = int(input('Input Your Converter Type:= '))
+    while b >= 3:
+        print('Invalid Number')
+        b = int(input('Input Your Converter Type Again:= '))
+    if b == 1:
+        x = int(input('Input Your Angle:= '))
+        x = x*h/180
+        print('Your Angle:=', x, 'rad')
+    elif b == 2:
+        x = int(input('Input Your Angle:= '))
+        x = x*180/h
+        print('Your Angle:=', x, "'")
+    print('''
+Main Menu   = 1
+Exit        = 2
+''')
+    y = int(input('Enter Your Number:= '))
+
